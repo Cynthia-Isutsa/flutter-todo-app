41c2678ac280263b9b06226a26d6fa3d2753afc2
Cynthia-Isutsa <isutsacynthia@gmail.com>
2024-12-08 21:37:03 +0300

    body

--- to_do_app/lib/pages/home_page.dart
+++ to_do_app/lib/pages/home_page.dart
@@ -1,19 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:to_do_app/utils/todo_list.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
    HomePage({super.key});
 
- 
-   
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final _controller = TextEditingController();
+ void checkBoxChanged (int index) {
+  setState(() {
+    toDoList[index][1] = !toDoList[index][1];
+  });
+    // toDoList[index][1] = !!toDoList[index][1];
+ }
+
+ void addNewTask(){
+    setState(() {
+      toDoList.add([_controller.text, false]);
+      _controller.clear();
+    });
+ }
 
   List toDoList = [
     ["Learn Flutter", false],
-    ["Learn Dart", false],
-    ["Learn React", false],
-    ["Learn Node", false],
+    ["Learn Java", false],
   ];
-  
+
   @override
   Widget build(BuildContext context){
     return Scaffold(
@@ -25,8 +40,72 @@ class HomePage extends StatelessWidget {
 
       ),
       body: ListView.builder(itemCount: toDoList.length, itemBuilder: (BuildContext context, index){
-          return TodoList()
-      })
+          return TodoList(
+            taskName: toDoList[index][0],
+             taskCompleted:toDoList[index][1], 
+            onChanged: (value){
+          checkBoxChanged(index);
+            }
+
+          );
+      }),
+      floatingActionButton: Row(
+        children: [
+          Expanded(child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: TextField(
+              controller: _controller ,
+              decoration: InputDecoration(
+                hintText: "Add new todo items",
+                filled: true,
+                fillColor: Colors.deepPurple.shade200,
+                enabledBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(color: Colors.white),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                 focusedBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(color: Colors.deepPurple),
+                  borderRadius: BorderRadius.circular(15),
+                )
+
+              ),
+            ),
+          )),
+          FloatingActionButton(
+            onPressed: addNewTask,
+            //   showDialog(
+            //     context: context,
+            //     builder: (context) {
+            //       return AlertDialog(
+            //         title: const Text('Add Task'),
+            //         content: const TextField(
+            //           autofocus: true,
+            //           decoration: const InputDecoration(
+            //             hintText: 'Enter Task Here',
+            //           ),
+            //         ),
+            //         actions: [
+            //           TextButton(
+            //             onPressed: () {
+            //               Navigator.of(context).pop();
+            //             },
+            //             child: const Text('Cancel'),
+            //           ),
+            //           TextButton(
+            //             onPressed: () {
+            //               Navigator.of(context).pop();
+            //             },
+            //             child: const Text('Add'),
+            //           ),
+            //         ],
+            //       );
+            //     },
+            //   );
+            // },
+            child: const Icon(Icons.add),
+          ),
+        ],
+      ),
     );
   }
 }
--- to_do_app/lib/utils/todo_list.dart
+++ to_do_app/lib/utils/todo_list.dart
@@ -1,34 +1,54 @@
 import "package:flutter/material.dart";
 
 class TodoList extends StatelessWidget {
-  const TodoList({super.key, required this.taskName});
+  const TodoList(
+      {super.key,
+      required this.taskName,
+      required this.taskCompleted,
+      this.onChanged});
 
-    final String taskName;
+  final String taskName;
+  final bool taskCompleted;
+  final Function(bool?)? onChanged;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-            padding: const EdgeInsets.only(
-              top: 20,
-              left: 20,
-              right: 20,
-              bottom: 0,
-            ),
-            child: Container (
-              padding: const EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                 color: Colors.deepPurple,
-                borderRadius: BorderRadius.circular(10),
+      padding: const EdgeInsets.only(
+        top: 20,
+        left: 20,
+        right: 20,
+        bottom: 0,
+      ),
+      child: Container(
+        padding: const EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          color: Colors.deepPurple,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Row(
+          children: [
+            Checkbox(
+              value: taskCompleted,
+              onChanged: onChanged, 
+              checkColor: Colors.black, 
+              side: const BorderSide(color: Colors.white, width: 2),
+              //fillColor: Colors.white, 
+              activeColor: Colors.white,
               ),
-             
-              child: Text(
-                taskName,
-                style: const TextStyle(
-                  fontSize: 20,
-                  color: Colors.white
-                ),
+            Text(
+              taskName,
+              style: TextStyle(
+                fontSize: 20, 
+                color: Colors.white,
+                decoration: taskCompleted ? TextDecoration.lineThrough : TextDecoration.none,
+                decorationColor: Colors.white,
+                decorationThickness: 2,
                 ),
             ),
-          );
+          ],
+        ),
+      ),
+    );
   }
 }
